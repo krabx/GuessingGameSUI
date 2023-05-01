59b8232cd6d4619eb7df9a686db90818dd2058a6
Bogdan Radchenko <inroph@mail.ru>
2023-05-01 15:18:49 +0300

    Initial Commit

---
+++ GuessingGameSUI/GuessingGameSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameSUIApp.swift
+//  GuessingGameSUI
+//
+//  Created by Богдан Радченко on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
