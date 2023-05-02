1f191d3f1ba900cc20ec671df2a606cc747e5f8c
Bogdan Radchenko <inroph@mail.ru>
2023-05-02 19:14:36 +0300

    adding modifier for buttons, adding background color, adding image

--- GuessingGameSUI/ContentView.swift
+++ GuessingGameSUI/ContentView.swift
@@ -13,33 +13,47 @@ struct ContentView: View {
     @State private var isAlertPresented = false
     
     var body: some View {
-        VStack(spacing: 20) {
-            HStack {
-                Text("Подвиньте слайдер, как можно ближе к: ")
-                
-                Text(targetValue.formatted())
-            }
-
-            HStack {
-                Text("0")
-                
-                UISliderRepresentation(
-                    targetValue: $targetValue,
-                    currentValue: $currentValue,
-                    result: computeScore()
-                )
-                
-                Text("100")
-            }
+        ZStack {
+            Color(red: 147 / 255, green: 197 / 255, blue: 114 / 255)
+                .ignoresSafeArea()
             
-            Button("Проверь меня!", action: { isAlertPresented.toggle()})
-                .alert("Ваш результат", isPresented: $isAlertPresented, actions: { }) {
-                    Text("\(computeScore()) очков")
+            VStack(spacing: 30) {
+                Image("logo")
+                    .resizable()
+                
+                HStack {
+                    Text("Подвиньте слайдер, как можно ближе к:")
+                    
+                    Text(targetValue.formatted())
                 }
-            
-            Button("Начать заново", action: setNewTargetValue)
+
+                HStack {
+                    Text("0")
+                    
+                    UISliderRepresentation(
+                        currentValue: $currentValue,
+                        result: computeScore()
+                    )
+                    
+                    Text("100")
+                }
+                
+                Button("Проверь меня!", action: { isAlertPresented.toggle() })
+                    .decor()
+                    .alert(
+                        "Ваш результат",
+                        isPresented: $isAlertPresented,
+                        actions: { }
+                    ) {
+                        Text("\(computeScore()) очков")
+                    }
+                
+                Button("Начать заново", action: setNewValues)
+                    .decor()
+            }
+            .padding()
         }
-        .padding()
+
     }
     
     private func computeScore() -> Int {
@@ -47,13 +61,37 @@ struct ContentView: View {
         return 100 - difference
     }
     
-    private func setNewTargetValue() {
+    private func setNewValues() {
         targetValue = Int.random(in: 0...100)
+        currentValue = Double.random(in: 0...100)
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonView: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 180, height: 50)
+            .foregroundColor(.white)
+            .font(.title2)
+            .background(.purple)
+            .cornerRadius(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 5)
+    }
+}
+
+extension Button {
+    func decor() -> some View {
+        modifier(ButtonView())
     }
 }
--- GuessingGameSUI/UISliderRepresentation.swift
+++ GuessingGameSUI/UISliderRepresentation.swift
@@ -8,25 +8,34 @@
 import SwiftUI
 
 struct UISliderRepresentation: UIViewRepresentable {
-    @Binding var targetValue: Int
     @Binding var currentValue: Double
     let result: Int
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
+        slider.minimumTrackTintColor = .purple
         slider.thumbTintColor = .red
         slider.minimumValue = 0
         slider.maximumValue = 100
         slider.value = Float(currentValue)
         
-        slider.addTarget(context.coordinator, action: #selector(Coordinator.valueChanged), for: .valueChanged)
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged),
+            for: .valueChanged
+        )
         
         return slider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
         uiView.value = Float(currentValue)
-        uiView.thumbTintColor = UIColor(red: 255 / 255, green: 0, blue: 0, alpha: Double(result) / 100)
+        uiView.thumbTintColor = UIColor(
+            red: 255 / 255,
+            green: 0,
+            blue: 0,
+            alpha: Double(result) / 100
+        )
     }
     
     func makeCoordinator() -> Coordinator {
@@ -50,6 +59,6 @@ extension UISliderRepresentation {
 
 struct UISliderRepresentation_Previews: PreviewProvider {
     static var previews: some View {
-        UISliderRepresentation(targetValue: .constant(50), currentValue: .constant(50), result: 50)
+        UISliderRepresentation(currentValue: .constant(50), result: 50)
     }
 }
